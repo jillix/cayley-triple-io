db6b42c62db15e94b430167e4d011d0b5dabf0d1
Adrian Ottiker <adrian@ottiker.com>
2016-09-03 15:26:03 +0300

    add cayley scripts

---
+++ scripts/start_cayley.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+APP_ROOT=`pwd`
+CAYLEY_PORT=64210
+
+CAYLEY_ROOT=bin/cayley
+DB_TYPE=mem
+CAYLEY_DEFAULT_DB=bin/testData.nq
+
+# check if a cayley instance is already running and kill it 
+CAYLEY_PROCESS_PID=`lsof -iTCP:$CAYLEY_PORT -sTCP:LISTEN -t`
+if [ "$CAYLEY_PROCESS_PID" ]
+then
+    echo "Found cayley process"
+else
+    # check if cayley is installed
+    if [ -e "$CAYLEY_ROOT/cayley" ]
+    then
+        echo "Found Cayley installation in: $CAYLEY_ROOT"
+    else
+        "$APP_ROOT/scripts/install_cayley.sh"
+    fi
+
+    echo "Using default"
+
+    # start cayley with default settings
+    cd "$CAYLEY_ROOT"
+    `./cayley http --dbpath=$APP_ROOT/$CAYLEY_DEFAULT_DB > /dev/null &`
+fi
